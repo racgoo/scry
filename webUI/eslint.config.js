ff1f50829ed08d1f3104a06756c187178192b6f8
racgoo <lhsung98@naver.com>
2025-07-02 20:42:10 +0900

    comment(Lint): add custom lint comment

--- webUI/eslint.config.js
+++ webUI/eslint.config.js
@@ -1,15 +1,18 @@
+import { globalIgnores } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh, { rules } from "eslint-plugin-react-refresh";
+import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import { globalIgnores } from "eslint/config";
 
+//Custom lint plugin
 import boundaries from "eslint-plugin-boundaries";
+//need "eslint-import-resolver-typescript": "^4.4.4" as dev dependency
 
 export default tseslint.config([
   globalIgnores(["dist"]),
   {
+    //Vite default code
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
@@ -17,23 +20,27 @@ export default tseslint.config([
       reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
+
+    //Scry custom lint code
     settings: {
+      //Resolve ts path(need "eslint-import-resolver-typescript" as dev dependency)
       "import/resolver": {
-        typescript: {},
+        typescript: {}, //automatically insert resolver
       },
+      //Define boundaries to use
       "boundaries/elements": [{ type: "features", pattern: "features/*" }],
     },
-
+    //Isolate features folder lint rule
     plugins: { boundaries },
     rules: {
       "boundaries/entry-point": [
-        2, //error flag
+        2, //Error flag(2: error, 1: warning, 0: off).. I think it's not good using error as number
         {
           default: "disallow",
           rules: [
             {
               target: ["features"],
-              allow: "index.ts",
+              allow: "index.ts", //Allow only index.ts file(Protect capsured module)
             },
           ],
         },
